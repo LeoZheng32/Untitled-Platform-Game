96d9c1f0bd87b207d515f8c1d4ac5f5754c2ba92
BT_1N3_06 <bths@edu>
2025-05-22 10:49:28 -0400

    Background

--- src/Frame.java
+++ src/Frame.java
@@ -2,14 +2,19 @@ import javax.swing.JFrame;
 
 public class Frame {
     JFrame frame;
+
     public Frame() {
         frame = new JFrame("Untitled Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(900, 600);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+
+        GraphicsPanel displayPanel = new GraphicsPanel();
+
+        frame.add(displayPanel);
 
         frame.setVisible(true);
+        //frame.revalidate();
 
     }
 }
--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -7,15 +7,69 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class GraphicsPanel {
+public class GraphicsPanel extends JPanel implements ActionListener, KeyListener, MouseListener {
     private BufferedImage background;
     private Timer timer;
 
     public GraphicsPanel() {
         try {
-            background = ImageIO.read(new File("src/background.png"));
+            background = ImageIO.read(new File("src/background.jpeg"));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        setFocusable(true);
+        requestFocusInWindow();
+        System.out.println(background);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
